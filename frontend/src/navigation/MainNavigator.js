e87e7b5ee543685588360c18afe6cb69f28d311c
Yevhen Shakhrai <hemlokceo@gmail.com>
2023-08-16 15:53:39 +0100

    updated navigator to have the landing page as a default one

--- frontend/src/navigation/MainNavigator.js
+++ frontend/src/navigation/MainNavigator.js
@@ -3,13 +3,15 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import HomeScreen from '../screens/HomeScreen';
 import AddExpenseScreen from '../screens/AddExpenseScreen';
+import LandingPage from '../screens/LandingPage';
 
 const Stack = createStackNavigator();
 
 function MainNavigator() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
+      <Stack.Navigator initialRouteName="LandingPage">
+        <Stack.Screen name="LandingPage" component={LandingPage} />
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="AddExpense" component={AddExpenseScreen} />
       </Stack.Navigator>
