f8fb7671a7a3575887a483017aa8017d7074e35d
Yevhen Shakhrai <hemlokceo@gmail.com>
2023-08-16 12:30:10 +0100

    finsihed the app script, add jwt token checker and error handling

--- api/app.js
+++ api/app.js
@@ -7,4 +7,33 @@ const JWT = require("jsonwebtoken");
 
 const app = express();
 
+const tokenChecker = (req, res, next) => {
+  let token;
+  const authHeader = req.get("Authorization");
+
+  if (authHeader) {
+    token = authHeader.slice(7);
+  }
+
+  JWT.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) {
+      res.status(401).json({ message: "Invalid token" });
+    } else {
+      req.decoded = decoded;
+      next();
+    }
+  });
+};
+
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+app.use((err, req, res) => {
+  res.locals.message = err.message;
+  res.loacals.error = req.app.get("env") === "development" ? err : {};
+
+  res.status(err.status || 500).json({ message: "server error" });
+});
+
 module.exports = app;
