129b9a521395ce20e12d3c98e03f46db6c0456cf
Yevhen Shakhrai <hemlokceo@gmail.com>
2023-08-16 12:24:14 +0100

    finsihed the www server script

--- api/bin/www.js
+++ api/bin/www.js
@@ -5,3 +5,54 @@ const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 8080;
 app.set("port", PORT);
+
+const MONGODB_URL =
+  process.env.MONGODB_URL || "mongodb://localhost:27017/pencepulse";
+mongoose.connect(MONGODB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+const server = http.createServer(app);
+
+server.listen(PORT);
+server.on("error", onError);
+server.on("listening", onListening);
+
+const normalizePort = (val) => {
+  const port = parseInt(val, 10);
+  if (isNaN(port)) return val;
+  if (port >= 0) return port;
+  return false;
+};
+
+const onError = (error) => {
+  if (error.syscall !== "listen") {
+    throw error;
+  }
+
+  const bind = typeof PORT === "string" ? `Pipe ${PORT}` : `Port ${PORT}`;
+
+  switch (error.code) {
+    case "EACCES":
+      console.error(`${bind} requires elevated privileges`);
+      process.exit(1);
+      break;
+    case "EADDRINUSE":
+      console.error(`${bind} is already in use`);
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+
+const onListening = () => {
+  const addr = server.address();
+  const bind = typeof addr === "string" ? `Pipe ${addr}` : `Port ${addr.port}`;
+  console.log(`Listening on ${bind}`);
+  debug(`Listening on ${bind}`);
+};
