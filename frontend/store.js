8b527847a11c9a9e98f33543aa9a8433145104b6
Yevhen Shakhrai <hemlokceo@gmail.com>
2023-08-16 20:19:16 +0100

    updated package with new libraries, store refactor

--- frontend/store.js
+++ frontend/store.js
@@ -1,4 +1,4 @@
-import { configureStore } from '@reduxjs/toolkit';
+import {configureStore} from '@reduxjs/toolkit';
 import expensesReducer from './reducers/expensesSlice';
 
 const store = configureStore({
