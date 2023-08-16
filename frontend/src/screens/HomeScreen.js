8411a6f5ddd0477c20028d645efd18e960ba26ad
Yevhen Shakhrai <hemlokceo@gmail.com>
2023-08-16 20:49:32 +0100

    add code to dispaly flow charts based on expenses and incomes

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,22 +1,70 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
-import { useSelector } from 'react-redux';
+import {View, Text, Button, StyleSheet, processColor} from 'react-native';
+import {useSelector} from 'react-redux';
+import {LineChart} from 'react-native-charts-wrapper';
 
-const HomeScreen = ({ navigation }) => {
-  const expenses = useSelector((state) => state.expenses.expenses);
+const HomeScreen = ({navigation}) => {
+  const expenses = useSelector(state => state.expenses.expenses);
+  const incomes = useSelector(state => state.expenses.incomes);
+
+  const sortedExpenses = expenses.sort(
+    (a, b) => new Date(a.date) - new Date(b.date),
+  );
+  const sortedIncomes = incomes.sort(
+    (a, b) => new Date(a.date) - new Date(b.date),
+  );
+
+  const expenseValues = sortedExpenses.map(expense => ({
+    y: parseFloat(expense.amount),
+  }));
+  const incomeValues = sortedIncomes.map(income => ({
+    y: parseFloat(income.amount),
+  }));
 
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Home Screen</Text>
-      {expenses.map((expense, index) => (
-        <Text key={index}>{expense.title}</Text>
-      ))}
+
+      <LineChart
+        style={styles.chart}
+        data={{
+          dataSets: [
+            {
+              label: 'Expenses',
+              values: expenseValues,
+              config: {
+                color: processColor('red'),
+              },
+            },
+            {
+              label: 'Incomes',
+              values: incomeValues,
+              config: {
+                color: processColor('green'),
+              },
+            },
+          ],
+        }}
+        chartDescription={{text: 'Financial Overview'}}
+        legend={{enabled: true}}
+      />
+
       <Button
-        title="Add Expense"
-        onPress={() => navigation.navigate('AddExpense')}
+        title="Add Transaction"
+        onPress={() => navigation.navigate('AddTransaction')}
       />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F5FCFF',
+  },
+  chart: {
+    flex: 1,
+  },
+});
 
 export default HomeScreen;
