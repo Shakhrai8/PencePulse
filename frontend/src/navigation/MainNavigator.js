f72b01772ed2932e41e1af34c5ed6d9e9559e904
Yevhen Shakhrai <hemlokceo@gmail.com>
2023-08-17 14:44:28 +0100

    new screens

--- frontend/src/navigation/MainNavigator.js
+++ frontend/src/navigation/MainNavigator.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import HomeScreen from '../screens/HomeScreen';
-import AddExpenseScreen from '../screens/AddExpenseScreen';
+import AddTransactionScreen from '../screens/AddTransactionScreen';
 import LandingScreen from '../screens/LandingScreen';
 import SignupScreen from '../screens/SignUpScreen';
 import LoginScreen from '../screens/LoginScreen';
@@ -15,7 +15,7 @@ function MainNavigator() {
       <Stack.Navigator initialRouteName="LandingScreen">
         <Stack.Screen name="LandingScreen" component={LandingScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="AddExpense" component={AddExpenseScreen} />
+        <Stack.Screen name="AddTransaction" component={AddTransactionScreen} />
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Signup" component={SignupScreen} />
       </Stack.Navigator>
