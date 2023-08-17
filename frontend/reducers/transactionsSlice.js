f284234e5647e805e2a6649d4545d31e38071382
Yevhen Shakhrai <hemlokceo@gmail.com>
2023-08-17 16:58:10 +0100

    updated redux to get the data from backend fetch

--- frontend/reducers/transactionsSlice.js
+++ frontend/reducers/transactionsSlice.js
@@ -4,13 +4,32 @@ export const transactionsSlice = createSlice({
   name: 'transactions',
   initialState: {
     transactions: [],
+    loading: false,
+    error: null,
   },
   reducers: {
     addTransaction: (state, action) => {
       state.transactions.push(action.payload);
     },
+    fetchTransactionBegin: state => {
+      state.loading = true;
+      state.error = null;
+    },
+    fetchTransactionSuccess: (state, action) => {
+      state.transactions = action.payload;
+      state.loading = false;
+    },
+    fetchTransactionError: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
   },
 });
 
-export const {addTransaction} = transactionsSlice.actions;
+export const {
+  addTransaction,
+  fetchTransactionBegin,
+  fetchTransactionSuccess,
+  fetchTransactionError,
+} = transactionsSlice.actions;
 export default transactionsSlice.reducer;
