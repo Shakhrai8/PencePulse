6e8d908aafbd4af30f5be9facd33e366f972f927
Yevhen Shakhrai <hemlokceo@gmail.com>
2023-08-21 19:03:14 +0100

    new controller to change passwords

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -28,6 +28,31 @@ const UserController = {
       return res.status(500).json({ message: "Internal server error" });
     }
   },
+  ChangePassword: async (req, res) => {
+    console.log("ChangePassword endpoint hit");
+    const { oldPassword, newPassword, userId } = req.body;
+
+    try {
+      const user = await User.findById(userId);
+
+      if (!user) {
+        return res.status(404).json({ message: "User not found" });
+      }
+
+      const isMatch = await user.isValidPassword(oldPassword);
+
+      if (!isMatch) {
+        return res.status(400).json({ message: "Old password is incorrect" });
+      }
+
+      user.password = await bcrypt.hash(newPassword, saltRounds);
+      await user.save();
+      res.status(200).json({ message: "Password updated successfully" });
+    } catch (err) {
+      console.error("Error changing password:", err);
+      res.status(500).json({ message: "Internal server error" });
+    }
+  },
 };
 
 module.exports = UserController;
