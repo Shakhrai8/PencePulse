a1093f80b2c0b018fe6f0cc03eef229ca2e2e9a0
Yevhen Shakhrai <hemlokceo@gmail.com>
2023-08-21 19:02:46 +0100

    updated user model with a new method to check if the password is the same

--- api/models/user.js
+++ api/models/user.js
@@ -1,10 +1,15 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const UserSchema = new mongoose.Schema({
   email: { type: String, required: true },
   password: { type: String, required: true },
   username: { type: String, required: true },
 });
+
+UserSchema.methods.isValidPassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
 
 const User = mongoose.model("User", UserSchema);
 
