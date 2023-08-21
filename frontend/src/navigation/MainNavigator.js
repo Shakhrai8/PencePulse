ed7114663823a530ddd0f64cbe9e682e66097487
Yevhen Shakhrai <hemlokceo@gmail.com>
2023-08-21 18:12:16 +0100

    upadted navigator to handle new profile screen

--- frontend/src/navigation/MainNavigator.js
+++ frontend/src/navigation/MainNavigator.js
@@ -7,6 +7,7 @@ import AddTransactionScreen from '../screens/AddTransactionScreen';
 import LandingScreen from '../screens/LandingScreen';
 import SignupScreen from '../screens/SignUpScreen';
 import LoginScreen from '../screens/LoginScreen';
+import ProfileScreen from '../screens/ProfileScreen';
 
 const Stack = createStackNavigator();
 
@@ -17,10 +18,15 @@ function MainNavigator() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName={initialScreen}>
-        <Stack.Screen name="LandingScreen" component={LandingScreen} />
+        <Stack.Screen
+          name="LandingScreen"
+          component={LandingScreen}
+          options={{headerShown: false}}
+        />
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="AddTransaction" component={AddTransactionScreen} />
         <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Profile" component={ProfileScreen} />
         <Stack.Screen name="Signup" component={SignupScreen} />
       </Stack.Navigator>
     </NavigationContainer>
