fce3699ff7d9774d38643ca098bc260e5fe27c99
Yevhen Shakhrai <hemlokceo@gmail.com>
2023-08-21 18:17:13 +0100

    updated redux to handle some bugs

--- frontend/reducers/authSlice.js
+++ frontend/reducers/authSlice.js
@@ -1,19 +1,19 @@
 import {createSlice} from '@reduxjs/toolkit';
 import jwt_decode from 'jwt-decode';
 
-const initialState = {
-  token: null,
-  userId: null,
-};
-
 export const authSlice = createSlice({
   name: 'auth',
   initialState,
   reducers: {
     setToken: (state, action) => {
-      state.token = action.payload;
-      const decodedToken = jwt_decode(action.payload);
-      state.userId = decodedToken.user_id;
+      if (action.payload) {
+        state.token = action.payload;
+        const decodedToken = jwt_decode(action.payload);
+        state.userId = decodedToken.user_id;
+      } else {
+        state.token = null;
+        state.userId = null;
+      }
     },
     logout: state => {
       state.token = null;
