8640a0a6e901b53af3aba9427585588b1b5bd879
Yevhen Shakhrai <hemlokceo@gmail.com>
2023-08-25 15:23:14 +0100

    expense chart will display tooltips when clicked/touched

--- frontend/src/components/ExpenseChart.js
+++ frontend/src/components/ExpenseChart.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {View, StyleSheet} from 'react-native';
-import {VictoryPie} from 'victory-native';
+import {VictoryPie, VictoryTooltip} from 'victory-native';
 
 const ExpenseChart = ({categoryExpenses}) => (
   <View style={styles.chartContainer}>
@@ -18,14 +18,19 @@ const ExpenseChart = ({categoryExpenses}) => (
       y="value"
       animate={{duration: 500}}
       height={350}
-      style={{
-        labels: {
-          fontSize: 14,
-          padding: 12,
-        },
-      }}
-      labelRadius={80}
-      labelPlacement="parallel"
+      labelComponent={
+        <VictoryTooltip
+          cornerRadius={5}
+          pointerLength={8}
+          flyoutStyle={{fill: 'white'}}
+          style={{fontSize: 12}}
+          renderInPortal={false}
+          flyoutPadding={{top: 10, bottom: 10, left: 15, right: 15}}
+        />
+      }
+      labelRadius={100}
+      labelPlacement="perpendicular"
+      padAngle={({datum}) => 2}
     />
   </View>
 );
