d33e69c154ad727ba5437f301f00ff84f17bdec9
Yevhen Shakhrai <hemlokceo@gmail.com>
2023-08-26 16:00:51 +0100

    add svg library into package, add new logo svg, updated profile screen with logo

--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -2,24 +2,23 @@ import React from 'react';
 import {View, Text, Button, StyleSheet} from 'react-native';
 import {useDispatch} from 'react-redux';
 import LogoutButton from '../components/LogoutButton';
+import LogoSvg from '../components/LogoSvg';
 
 const ProfileScreen = ({navigation}) => {
   const dispatch = useDispatch();
 
   return (
     <View style={styles.container}>
+      <LogoSvg width={300} height={300} />
       <Text style={styles.header}>Profile</Text>
-
       <Button
         title="Change Password"
         onPress={() => navigation.navigate('ChangePassword')}
       />
-
       <Button
         title="Edit Profile Details"
         onPress={() => navigation.navigate('EditProfile')}
       />
-
       <LogoutButton navigation={navigation} />
     </View>
   );
@@ -28,10 +27,10 @@ const ProfileScreen = ({navigation}) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
-    padding: 20,
+    padding: 0,
   },
   header: {
     fontSize: 20,
