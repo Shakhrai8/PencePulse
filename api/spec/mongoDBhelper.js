c4ce5b14b3492ea6b557263af14fc4b0af568f9b
Yevhen Shakhrai <hemlokceo@gmail.com>
2023-08-27 10:51:40 +0100

    installed supertest, add new test units for token controller

---
+++ api/spec/mongoDBhelper.js
@@ -0,0 +1,18 @@
+var mongoose = require("mongoose");
+
+beforeAll(function (done) {
+  mongoose.connect("mongodb://0.0.0.0/pencepulse_test", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  var db = mongoose.connection;
+  db.on("error", console.error.bind(console, "MongoDB connection error:"));
+  db.on("open", function () {
+    done();
+  });
+});
+
+afterAll(async () => {
+  await mongoose.connection.close();
+});
