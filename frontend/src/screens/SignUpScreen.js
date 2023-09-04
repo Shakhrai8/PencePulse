44ee53251cc6ffdca5fe9042b6e82b9055dad5b0
Yevhen Shakhrai <hemlokceo@gmail.com>
2023-09-04 18:55:10 +0100

    add checkers for email and password

--- frontend/src/screens/SignUpScreen.js
+++ frontend/src/screens/SignUpScreen.js
@@ -7,8 +7,31 @@ const SignupScreen = ({navigation}) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
+  const isEmailValid = email => {
+    const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    return emailPattern.test(email);
+  };
+
+  const isStrongPassword = password => {
+    const passwordPattern =
+      /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9]).{8,}$/;
+    return passwordPattern.test(password);
+  };
+
   const handleSignup = async () => {
     try {
+      if (!isEmailValid(email)) {
+        Alert.alert('Error', 'Please enter a valid email address');
+        return;
+      }
+
+      if (!isStrongPassword(password)) {
+        Alert.alert(
+          'Error',
+          'Password must contain at least 8 characters, including one uppercase letter, one lowercase letter, one number, and one special character',
+        );
+        return;
+      }
       const response = await api.post('/signup', {email, password, username});
 
       if (response.status === 201) {
@@ -31,14 +54,14 @@ const SignupScreen = ({navigation}) => {
         value={username}
         onChangeText={setUsername}
         style={styles.input}
-        testID='username-input'
+        testID="username-input"
       />
       <TextInput
         placeholder="Email"
         value={email}
         onChangeText={setEmail}
         style={styles.input}
-        testID='email-input'
+        testID="email-input"
       />
       <TextInput
         placeholder="Password"
@@ -46,7 +69,7 @@ const SignupScreen = ({navigation}) => {
         onChangeText={setPassword}
         secureTextEntry
         style={styles.input}
-        testID='password-input'
+        testID="password-input"
       />
       <Button title="Sign Up" onPress={handleSignup} testID="signup-btn" />
     </View>
